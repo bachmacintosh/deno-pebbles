2d2aaa6cf89ee6979b0c4cda8bb1ce465a857c0b
Collin Bachman <collin@bachman.io>
2022-12-24 15:03:17 -0500

    Don't use tabs
    Signed-off-by: Collin Bachman <collin@bachman.io>

--- google-oauth/functions/github/encryptAndStoreRefreshToken.ts
+++ google-oauth/functions/github/encryptAndStoreRefreshToken.ts
@@ -3,35 +3,35 @@ import getRepoPublicKey from "./getRepoPublicKey.ts";
 import { sodium } from "../../deps.ts";
 
 export default async function encryptAndStoreRefreshToken(
-	gitHubJson: GitHubCredentialsJson,
-	accessToken: GoogleAccessTokenJson,
+  gitHubJson: GitHubCredentialsJson,
+  accessToken: GoogleAccessTokenJson,
 ): Promise<void> {
-	await sodium.ready.then(async () => {
-		const publicKey = await getRepoPublicKey(gitHubJson);
-		const binKey = sodium.from_base64(publicKey.key, 1);
-		const binSecret = sodium.from_string(JSON.stringify(accessToken.refresh_token));
-		const encryptedBytes = sodium.crypto_box_seal(binSecret, binKey);
-		const encryptedString = sodium.to_base64(encryptedBytes, 1);
+  await sodium.ready.then(async () => {
+    const publicKey = await getRepoPublicKey(gitHubJson);
+    const binKey = sodium.from_base64(publicKey.key, 1);
+    const binSecret = sodium.from_string(JSON.stringify(accessToken.refresh_token));
+    const encryptedBytes = sodium.crypto_box_seal(binSecret, binKey);
+    const encryptedString = sodium.to_base64(encryptedBytes, 1);
 
-		const url =
-			`https://api.github.com/repos/${gitHubJson.owner}/${gitHubJson.repo}/actions/secrets/GOOGLE_REFRESH_TOKEN`;
-		const init: RequestInit = {
-			method: "PUT",
-			headers: new Headers({
-				Authorization: `Bearer ${gitHubJson.accessToken}`,
-				Accept: "application/vnd.github+json",
-				"X-GitHub-Api-Version": "2022-11-28",
-			}),
-			body: JSON.stringify(
-				{
-					"encrypted_value": encryptedString,
-					"key_id": publicKey.key_id,
-				},
-			),
-		};
-		const response = await fetch(url, init);
-		if (!response.ok) {
-			throw new Error(`Could not encrypt and put GitHub Action Secret ${response.status}`);
-		}
-	});
+    const url =
+      `https://api.github.com/repos/${gitHubJson.owner}/${gitHubJson.repo}/actions/secrets/GOOGLE_REFRESH_TOKEN`;
+    const init: RequestInit = {
+      method: "PUT",
+      headers: new Headers({
+        Authorization: `Bearer ${gitHubJson.accessToken}`,
+        Accept: "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+      }),
+      body: JSON.stringify(
+        {
+          "encrypted_value": encryptedString,
+          "key_id": publicKey.key_id,
+        },
+      ),
+    };
+    const response = await fetch(url, init);
+    if (!response.ok) {
+      throw new Error(`Could not encrypt and put GitHub Action Secret ${response.status}`);
+    }
+  });
 }
--- google-oauth/functions/util/openAuthorizationWindow.ts
+++ google-oauth/functions/util/openAuthorizationWindow.ts
@@ -2,24 +2,24 @@ import type { GoogleCredentialsJson } from "../../types.ts";
 import { open } from "../../deps.ts";
 
 export default function openAuthorizationWindow(
-	googleJson: GoogleCredentialsJson,
-	scopes: string[],
-	state: string,
+  googleJson: GoogleCredentialsJson,
+  scopes: string[],
+  state: string,
 ): void {
-	const url = [
-		"https://accounts.google.com/o/oauth2/v2/auth",
-		`?client_id=${encodeURIComponent(googleJson.web.client_id)}`,
-		`&redirect_uri=${encodeURIComponent(googleJson.web.redirect_uris[0])}`,
-		`&scope=${encodeURIComponent(scopes.join(" "))}`,
-		`&state=${state}`,
-		"&response_type=code",
-		"&access_type=offline",
-		"&include_granted_scopes=true",
-		"&prompt=consent",
-	].join("");
-	try {
-		open(url);
-	} catch (_error) {
-		console.info(`If your browser did not open, go to this URL to authorize with Google: ${url}`);
-	}
+  const url = [
+    "https://accounts.google.com/o/oauth2/v2/auth",
+    `?client_id=${encodeURIComponent(googleJson.web.client_id)}`,
+    `&redirect_uri=${encodeURIComponent(googleJson.web.redirect_uris[0])}`,
+    `&scope=${encodeURIComponent(scopes.join(" "))}`,
+    `&state=${state}`,
+    "&response_type=code",
+    "&access_type=offline",
+    "&include_granted_scopes=true",
+    "&prompt=consent",
+  ].join("");
+  try {
+    open(url);
+  } catch (_error) {
+    console.info(`If your browser did not open, go to this URL to authorize with Google: ${url}`);
+  }
 }
