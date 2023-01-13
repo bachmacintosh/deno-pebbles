b2615b0b3f1484fd9eadf1ff6d5651763842dd87
Collin Bachman <collin@bachman.io>
2023-01-12 20:30:19 -0500

    Bump std to 0.171.0
    Signed-off-by: Collin Bachman <collin@bachman.io>

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { join } from "https://deno.land/std@0.168.0/path/mod.ts";
+export { join } from "https://deno.land/std@0.171.0/path/mod.ts";
 export { open } from "https://deno.land/x/open@v0.0.5/index.ts";
 import sodium from "https://deno.land/x/sodium@0.2.0/basic.ts";
 
