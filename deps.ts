a310869f72560bf43c3f5e0b5d42db97608e63b5
Collin Bachman <collin.r68zdn9d@bachman.dev>
2023-06-19 16:33:42 -0400

    Update dependencies
    Signed-off-by: Collin Bachman <collin.r68zdn9d@bachman.dev>

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export { join } from "https://deno.land/std@0.171.0/path/mod.ts";
-export { open } from "https://deno.land/x/open@v0.0.5/index.ts";
+export { join } from "https://deno.land/std@0.192.0/path/mod.ts";
+export { open } from "https://deno.land/x/open@v0.0.6/index.ts";
 import sodium from "https://deno.land/x/sodium@0.2.0/basic.ts";
 
 export { sodium };
